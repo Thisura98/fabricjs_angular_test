d8a8cc818b67de363dd019a2cb192f4a0bb460e7
Thisura98 <thisura1998@gmail.com>
2021-09-02 01:00:52 +0530

    Integrated Fabric JS Basic Canvas events

--- fabrictest/src/app/test/test.component.ts
+++ fabrictest/src/app/test/test.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { fabric } from 'fabric';
 
 @Component({
   selector: 'app-test',
@@ -7,9 +10,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  constructor() { }
+  canvas: fabric.Canvas | undefined;
+  events: string[] = [];
+
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.canvas = new fabric.Canvas('fabricSurface', {
+      backgroundColor: '#010101',
+      selection: true
+    })
+
+    this.setup();
+  }
+
+  testNavClicked(){
+    this.router.navigate(['/']);
+  }
+
+  private setup(){
+    const rect = new fabric.Rect({ 
+      width: 100, 
+      height: 50, 
+      fill: 'green',
+      hasBorders: true,
+      borderColor: '#00AAFF',
+      borderDashArray: [1,2,0],
+      borderScaleFactor: 2
+    });
+    this.canvas!.on('selection:created', (e) => {
+      this.events.push(`Selected ${e.target!.type!}`);
+    });
+    this.canvas!.on('selection:updated', (e) => {
+      this.events.push(`Selectede ${e.target!.type!}`);
+    });
+    this.canvas!.on('selection:cleared', (e) => {
+      this.events.push('Selection cleared');
+    });
+    // rect.on('selection:cleared', () => {
+    //   this.events.push('Selected Rect');
+    // });
+
+    this.canvas?.add(rect);
+
+    const img = fabric.Image.fromURL(
+      'http://localhost/fs/res_upload/image/1630448440504.png', 
+      (fabricImage) => {
+        fabricImage.scaleToWidth(100);
+        this.canvas?.add(fabricImage);
+
+        fabricImage.on('selection:created', () => {
+          this.events.push('Selected Image');
+        })
+        fabricImage.on('selection:cleared', () => {
+          this.events.push('Selected Image');
+        })
+    });
   }
 
 }
